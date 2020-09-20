dbb63c47a166255dc3c3329ca505aa2d70765084
negrovariant <sixto.cea1987@gmail.com>
2020-09-20 18:36:24 -0300

    index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # get 'landing', to: "landings#home"
   get 'landing', to: "landings#home", as: 'paginas'
   get 'autenticar', to: "landings#autenticar"
+  root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
